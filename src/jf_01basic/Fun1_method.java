10e67a24c9b91bcd87d3abd3a081bf5d8bbdee22
Sanjay Sunar <sansunar11@gmail.com>
2022-11-24 17:48:44 +0530

    Create Fun1_method.java

---
+++ src/jf_01basic/Fun1_method.java
@@ -0,0 +1,14 @@
+package jf_01basic;
+
+public class Fun1_method {
+    public static void main(String[] args) {
+        fun();
+        fun();
+    }
+
+    private static void fun() {
+        int x = 5, y = 10;
+        System.out.println(x + y);
+    }
+
+}
