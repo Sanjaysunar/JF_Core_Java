b9ca1493c696e795074e842fe127a2a35f1efd61
Sanjay Sunar <sansunar11@gmail.com>
2022-11-25 22:24:55 +0530

    Create methodTest2.java

---
+++ src/jf_01basic/methodTest2.java
@@ -0,0 +1,13 @@
+package jf_01basic;
+
+public class methodTest2 {
+    public static void main(String[] args) {
+        int x = 5;
+        fun(x);
+        System.out.println(x);
+    }
+
+    private static void fun(int x) {
+        x = x + 5;
+    }
+}
