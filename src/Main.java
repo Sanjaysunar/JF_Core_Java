b8ce664643b4233df3fbe116da61d26f9dfbdb01
Sanjay Sunar <sansunar11@gmail.com>
2022-11-30 19:05:22 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int five = 5;
         int two = 2;
-        int total = five + (five > 6 ? ++two : two--);
+        int total = five + (five > 6 ? ++two : --two);
         System.out.println(total);
     }
 }
