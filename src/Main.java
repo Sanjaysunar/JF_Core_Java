4b554367c2e8a787dc2ffd335c39caec917ffee0
Sanjay Sunar <sansunar11@gmail.com>
2022-11-30 19:05:48 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-public class Main {
+class Test
+{
+    int i,j;
+}
+class Main{
     public static void main(String[] args) {
-        int five = 5;
-        int two = 2;
-        int total = five + (five > 3 ? ++two : --two);
-        System.out.println(total);
+        Test t;
+        System.out.println(t.i);
+        System.out.println(t.j);
     }
 }
